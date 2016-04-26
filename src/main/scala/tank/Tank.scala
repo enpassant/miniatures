f67ce4a1488e82c806d6c6ce761452336637c813
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2016-04-26 11:04:27 +0200

    Add Tank sample

---
+++ src/main/scala/tank/Tank.scala
@@ -0,0 +1,20 @@
+import scala.math._
+
+case class Tank(x: Int, y: Int, d: Int = 0) {
+  def előre(l: Int) = Tank(
+    (x + cos(Pi * d / 180) * l).toInt,
+    (y + sin(Pi * d / 180) * l).toInt,
+    d)
+
+  def hátra(l: Int) = előre(-l)
+
+  def fordul(dr: Int) = Tank(x, y, (d + dr) % 360)
+}
+
+object Tank extends App {
+  def balra = 90
+  def jobbra = -90
+
+  val tank = Tank(50, 50) fordul balra előre 10 fordul jobbra hátra 10
+  println(tank)
+}
