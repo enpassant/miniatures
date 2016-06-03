37263d5c1285929b0082ed6587380d0c613fde1c
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2016-06-03 10:43:21 +0200

    Add Point member to Shape

--- src/main/scala/shape/ShapeOO.scala
+++ src/main/scala/shape/ShapeOO.scala
@@ -4,14 +4,15 @@ object ShapeOO {
   case class Point(x: Int, y: Int)
 
   sealed trait Shape {
-    def draw(point: Point): String
+    def point: Point
+    def draw: String
     def sideCount(): Int
     def removeSmoothing(): Shape
     def reform(clazz: Class[_]): Option[Shape]
   }
 
-  case class Circle(radius: Int) extends Shape {
-    def draw(point: Point): String =
+  case class Circle(point: Point, radius: Int) extends Shape {
+    def draw: String =
       s"Drawing circle at (${point.x}, ${point.y}) Radius: $radius"
 
     def sideCount() = 1
@@ -22,23 +23,23 @@ object ShapeOO {
       if (getClass == Circle.getClass)
         Some(this)
       else if (clazz == Ellipse.getClass)
-        Some(Ellipse(radius*2, radius*2))
+        Some(Ellipse(point, radius*2, radius*2))
       else if (clazz == Rectangle.getClass)
         reform(Ellipse.getClass) flatMap { ellipse => ellipse.reform(Rectangle.getClass) }
       else None
   }
 
-  case class Rectangle(width: Int, height: Int, smoothing: Int = 0) extends Shape {
-    def draw(point: Point): String = smoothing match {
-      case 0 => drawRectangle(point)
-      case 100 => Ellipse(width, height).draw(point)
-      case _ => drawRoundedRectangle(point)
+  case class Rectangle(point: Point, width: Int, height: Int, smoothing: Int = 0) extends Shape {
+    def draw: String = smoothing match {
+      case 0 => drawRectangle
+      case 100 => Ellipse(point, width, height).draw
+      case _ => drawRoundedRectangle
     }
 
-    private def drawRectangle(point: Point): String =
+    private def drawRectangle: String =
       s"Drawing rectangle at (${point.x}, ${point.y}) Width $width, Height $height"
 
-    private def drawRoundedRectangle(point: Point): String =
+    private def drawRoundedRectangle: String =
       s"Drawing rounded rectangle at (${point.x}, ${point.y}) " +
         s"Width $width, Height $height, Smoothing $smoothing"
 
@@ -50,14 +51,14 @@ object ShapeOO {
       if (getClass == Rectangle.getClass)
         Some(this)
       else if (clazz == Ellipse.getClass && smoothing == 100)
-        Some(Ellipse(width, height))
+        Some(Ellipse(point, width, height))
       else if (clazz == Circle.getClass)
         reform(Ellipse.getClass) flatMap { ellipse => ellipse.reform(Circle.getClass) }
       else None
   }
 
-  case class Ellipse(width: Int, height: Int) extends Shape {
-    def draw(point: Point): String =
+  case class Ellipse(point: Point, width: Int, height: Int) extends Shape {
+    def draw: String =
       s"Drawing ellipse at (${point.x}, ${point.y}) Width $width, Height $height"
 
     def sideCount() = 1
@@ -68,9 +69,9 @@ object ShapeOO {
       if (getClass == Ellipse.getClass)
         Some(this)
       else if (clazz == Rectangle.getClass)
-        Some(Rectangle(width, height, 100))
+        Some(Rectangle(point, width, height, 100))
       else if (clazz == Circle.getClass && width == height)
-        Some(Circle(width / 2))
+        Some(Circle(point, width / 2))
       else None
   }
 }
