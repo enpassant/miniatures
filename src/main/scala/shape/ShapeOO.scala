5f18cd78d76c5ad26a10317079dd0c15f9160f10
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2016-06-07 18:41:38 +0200

    Add amount to ShapeOO

--- src/main/scala/shape/ShapeOO.scala
+++ src/main/scala/shape/ShapeOO.scala
@@ -9,19 +9,26 @@ object ShapeOO {
 
   sealed trait Shape {
     def point: Point
-    def draw(device: Device): String = this.draw(device)
-    def draw(display: Display): String
-    def draw(printer: Printer): String
+    def draw(device: Device, amount: Int): String = this.draw(device, amount)
+    def draw(display: Display, amount: Int): String
+    def draw(printer: Printer, amount: Int): String
     def sideCount(): Int
     def removeSmoothing(): Shape
     def reform(clazz: Class[_]): Option[Shape]
   }
 
   case class Circle(point: Point, radius: Int) extends Shape {
-    def draw(display: Display): String =
-      s"Drawing circle at (${point.x}, ${point.y}) Radius: $radius"
+    def draw(display: Display, amount: Int): String = {
+      if (amount < 50) {
+        s"Minimum amount is 50, you have only $amount"
+      } else if (amount >= 500) {
+        s"Drawing expensive circle at (${point.x}, ${point.y}) Radius: $radius"
+      } else {
+        s"Drawing circle at (${point.x}, ${point.y}) Radius: $radius"
+      }
+    }
 
-    def draw(printer: Printer): String =
+    def draw(printer: Printer, amount: Int): String =
       s"Printing circle at (${point.x}, ${point.y}) Radius: $radius"
 
     def sideCount() = 1
@@ -39,17 +46,19 @@ object ShapeOO {
   }
 
   case class Rectangle(point: Point, width: Int, height: Int, smoothing: Int = 0) extends Shape {
-    def draw(display: Display): String =
-      smoothing match {
+    def draw(display: Display, amount: Int): String =
+      if (amount < 50) {
+        s"Minimum amount is 50, you have only $amount"
+      } else smoothing match {
         case 0 => drawRectangle(display)
-        case 100 => Ellipse(point, width, height).draw(display)
+        case 100 => Ellipse(point, width, height).draw(display, amount)
         case _ => drawRoundedRectangle(display)
       }
 
-    def draw(printer: Printer): String =
+    def draw(printer: Printer, amount: Int): String =
       smoothing match {
         case 0 => printRectangle(printer)
-        case 100 => Ellipse(point, width, height).draw(printer)
+        case 100 => Ellipse(point, width, height).draw(printer, amount)
         case _ => printRoundedRectangle(printer)
       }
 
@@ -82,10 +91,14 @@ object ShapeOO {
   }
 
   case class Ellipse(point: Point, width: Int, height: Int) extends Shape {
-    def draw(display: Display): String =
-      s"Drawing ellipse at (${point.x}, ${point.y}) Width $width, Height $height"
+    def draw(display: Display, amount: Int): String =
+      if (amount < 50) {
+        s"Minimum amount is 50, you have only $amount"
+      } else {
+        s"Drawing ellipse at (${point.x}, ${point.y}) Width $width, Height $height"
+      }
 
-    def draw(printer: Printer): String =
+    def draw(printer: Printer, amount: Int): String =
       s"Printing ellipse at (${point.x}, ${point.y}) Width $width, Height $height"
 
     def sideCount() = 1
