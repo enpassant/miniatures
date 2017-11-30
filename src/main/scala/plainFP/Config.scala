e3cba54ce73419081520b493325190a9fdf546bf
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2017-11-30 16:18:05 +0100

    Add better solution for readPerson

--- src/main/scala/plainFP/Config.scala
+++ src/main/scala/plainFP/Config.scala
@@ -23,12 +23,23 @@ object ConfigAPI {
       age <- readAge(config.age)
     } yield Person(name, age)
   }
+
+  // or better:
+  def optMap2[A, B, C](f: (A, B) => C)(optA: Option[A], optB: Option[B]) =
+    for {a <- optA; b <- optB} yield f(a, b)
 }
 
 object ConfigApp extends App {
   import ConfigAPI._
 
-  println(readPerson(Config("John Doe", 20)))
-  println(readPerson(Config("Incognito", 99)))
-  println(readPerson(Config("John Doe", 170)))
+  val config1 = Config("John Doe", 20)
+  println(readPerson(config1))
+  println(optMap2(Person)(readName(config1.name), readAge(config1.age)))
+  println(optMap2(Tuple2[Name, Age])(readName(config1.name), readAge(config1.age)))
+  val config2 = Config("Incognito", 99)
+  println(readPerson(config2))
+  println(optMap2(Person)(readName(config2.name), readAge(config2.age)))
+  val config3 = Config("John Doe", 170)
+  println(readPerson(config3))
+  println(optMap2(Person)(readName(config3.name), readAge(config3.age)))
 }
