af3bb2e24cef98e46f199f2eb7a2f43023e71278
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2018-05-01 10:07:02 +0200

    Refactor FuncDI to more functional

--- src/main/scala/com/example/FuncDI.scala
+++ src/main/scala/com/example/FuncDI.scala
@@ -1,60 +1,88 @@
 package com.example
 
+import cats.effect.IO
+import cats.data.EitherT
+import cats.implicits._
+
+sealed trait Error
+
+case class MissingItem(id: ItemID) extends Error
+case object NoSpaceOnDisk extends Error
+
+object result {
+  type Result[A] = EitherT[IO, Error, A]
+
+  def makeLogged[I, O](fn: I => Result[O])(input: I): Result[O] = {
+    println(s"LOG. input: $input")
+    fn(input).flatMap { output: O =>
+      println(s"LOG. output: $output")
+      EitherT.rightT(output)
+    }
+  }
+}
+import result._
+
+case class ItemID(id: String) extends AnyVal
+case class OrderID(id: String) extends AnyVal
+
 case class Item(name: String, price: Double)
-case class OrderedItem(itemId: String, quantity: Double)
-case class Order(id: String, customer: String, items: List[OrderedItem])
+case class OrderedItem(itemId: ItemID, quantity: Double)
+case class Order(id: OrderID, customer: String, items: List[OrderedItem])
 
 object StoreAPI {
   case class OrderRequest(order: Order)
-  case class OrderResponse(status: Either[String, Double])
+  case class OrderResponse(status: Either[Error, Double])
 
-  type PlaceOrder = OrderRequest => OrderResponse
+  type PlaceOrder = OrderRequest => Result[OrderResponse]
 }
 
 object DatabaseAPI {
-  type GetItem = String => Either[String, Item]
-  type SaveOrder = Order => Either[String, Order]
+  type GetItem = ItemID => Result[Item]
+  type SaveOrder = Order => Result[Order]
 }
 
 object MemDatabase {
   import DatabaseAPI._
 
   val items = Map(
-    "001" -> Item("Sword", 399.9),
-    "002" -> Item("Chair", 24.9),
-    "003" -> Item("Table", 99.9),
-    "004" -> Item("Bed", 59.9)
+    ItemID("001") -> Item("Sword", 399.9),
+    ItemID("002") -> Item("Chair", 24.9),
+    ItemID("003") -> Item("Table", 99.9),
+    ItemID("004") -> Item("Bed", 59.9)
   )
 
-  val getItem: GetItem = id => items get id match {
-    case None => Left(s"Missing item $id!")
-    case Some(item) => Right(item)
+  val getItem: GetItem = id => {
+    items get id match {
+      case None => EitherT.leftT(MissingItem(id))
+      case Some(item) => EitherT.rightT(item)
+    }
   }
-  val saveOrder: SaveOrder = order => Right(order)
-  val saveOrderFailing: SaveOrder = order => Left("No space on disk!")
+  val saveOrder: SaveOrder = order => EitherT.rightT(order)
+  val saveOrderFailing: SaveOrder = order => EitherT.leftT(NoSpaceOnDisk)
 }
 
 object Store {
   import DatabaseAPI._
   import StoreAPI._
 
-  private def sequence[A, B](s: Seq[Either[A, B]]): Either[A, Seq[B]] =
-    s.foldRight(Right(Nil): Either[A, List[B]]) {
-      (e, acc) => for (xs <- acc.right; x <- e.right) yield x :: xs
-    }
+  val getItems: GetItem => OrderRequest => Result[List[(Item, OrderedItem)]] = {
+    getItem => request =>
+    (request.order.items.map {
+      orderedItem => getItem(orderedItem.itemId).map((_, orderedItem))
+    }).sequence
+  }
 
-  val placeOrderBusinessLogic =
-    (getItem: GetItem, saveOrder: SaveOrder) =>
-      (request: OrderRequest) =>
+  val calcPrice: (Item, OrderedItem) => Double = (item, orderedItem) =>
+    item.price * orderedItem.quantity
+
+  val placeOrderBusinessLogic: (GetItem, SaveOrder) => PlaceOrder =
   {
-      val sumList = request.order.items.map {
-        orderedItem => getItem(orderedItem.itemId).right map {
-          _.price * orderedItem.quantity }
-      }
-      val savedOrder = sequence(sumList).right flatMap {
-        sl => saveOrder(request.order).right map (_ => sl.sum)
-      }
-      OrderResponse(savedOrder)
+    (getItem, saveOrder) => request =>
+    for {
+      itemList <- getItems(getItem)(request)
+      priceList = itemList.map(calcPrice.tupled)
+      _ <- saveOrder(request.order)
+    } yield OrderResponse(Right(priceList.sum))
   }
 }
 
@@ -66,48 +94,40 @@ object FuncDI extends App {
   import DatabaseAPI._
   import MemDatabase._
 
-  var config: Config = _
+  val config: Config = Config(false)
 
   lazy val getItemFn =
     if (config.logged) makeLogged(getItem) _
     else getItem
 
-  val orderRequest1 = OrderRequest(Order("ORD_01", "IBM",
+  val orderRequest1 = OrderRequest(Order(OrderID("ORD_01"), "IBM",
     List(
-      OrderedItem("002", 12.0),
-      OrderedItem("004", 2.0)
+      OrderedItem(ItemID("002"), 12.0),
+      OrderedItem(ItemID("004"), 2.0)
     )))
 
-  val orderRequest2 = OrderRequest(Order("ORD_02", "Google",
+  val orderRequest2 = OrderRequest(Order(OrderID("ORD_02"), "Google",
     List(
-      OrderedItem("002", 12.0),
-      OrderedItem("005", 2.0)
+      OrderedItem(ItemID("002"), 12.0),
+      OrderedItem(ItemID("005"), 2.0)
     )))
-
-  config = Config(false)
 
   val placeOrderMemDB = placeOrderBusinessLogic(getItemFn, saveOrder)
 
-  println(placeOrderMemDB(orderRequest1).toString)
-  println(placeOrderMemDB(orderRequest2).toString)
+  println(placeOrderMemDB(orderRequest1).value.unsafeRunSync.toString)
+  println(placeOrderMemDB(orderRequest2).value.unsafeRunSync.toString)
 
   val placeOrderMemDBFailing = placeOrderBusinessLogic(
     getItem,
     saveOrderFailing)
 
-  println(placeOrderMemDBFailing(orderRequest1).toString)
-  println(placeOrderMemDBFailing(orderRequest2).toString)
+  println(placeOrderMemDBFailing(orderRequest1).value.unsafeRunSync.toString)
+  println(placeOrderMemDBFailing(orderRequest2).value.unsafeRunSync.toString)
 
-  def makeLogged[I, O](fn: I => O)(input: I): O = {
-    println(s"LOG. input: $input")
-    val output = fn(input)
-    println(s"LOG. output: $output")
-    output
-  }
   val placeOrderLogged = placeOrderBusinessLogic(
     makeLogged(getItem),
     makeLogged(saveOrder))
 
-  println(placeOrderLogged(orderRequest1).toString)
-  println(placeOrderLogged(orderRequest2).toString)
+  println(placeOrderLogged(orderRequest1).value.unsafeRunSync.toString)
+  println(placeOrderLogged(orderRequest2).value.unsafeRunSync.toString)
 }
